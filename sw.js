86f0c3accd36478fc115e67aa5214a7eb2c7b8f6
Dennis Dang <dangggdennis@gmail.com>
2018-06-19 23:37:42 -0700

    cleaning up sw.js

--- sw.js
+++ sw.js
@@ -65,12 +65,6 @@ self.addEventListener("fetch", function(event) {
             }),
         );
     }
-
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        }),
-    );
 });
 
 function servePhoto(request) {
