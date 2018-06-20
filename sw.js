c7d955c79942e85e65bace47c22aa808f6d56bd5
Dennis Dang <dangggdennis@gmail.com>
2018-06-19 23:29:57 -0700

    fix: precommitting images

--- sw.js
+++ sw.js
@@ -15,6 +15,16 @@ self.addEventListener("install", function(event) {
                 "css/styles.css",
                 "data/restaurants.json",
                 "restaurant.html",
+                "img/1.jpg",
+                "img/2.jpg",
+                "img/3.jpg",
+                "img/4.jpg",
+                "img/5.jpg",
+                "img/6.jpg",
+                "img/7.jpg",
+                "img/8.jpg",
+                "img/9.jpg",
+                "img/10.jpg",
             ]);
         }),
     );
