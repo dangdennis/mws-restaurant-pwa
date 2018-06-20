6601e0cdc2982a892bf625f0c9918644266668d6
Dennis Dang <dangggdennis@gmail.com>
2018-06-19 23:42:57 -0700

    more sw work

--- sw.js
+++ sw.js
@@ -58,12 +58,6 @@ self.addEventListener("fetch", function(event) {
             event.respondWith(caches.match("restaurant.html"));
             return;
         }
-
-        event.respondWith(
-            caches.match(event.request).then(function(response) {
-                return response || fetch(event.request);
-            }),
-        );
     }
 });
 
