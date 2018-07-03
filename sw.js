df381f8b3d866554ca8d8099de6ca4297952ddd2
Dennis Dang <dangggdennis@gmail.com>
2018-07-03 16:14:09 -0700

    fix cache files

--- sw.js
+++ sw.js
@@ -3,12 +3,22 @@
 // Default files to always cache
 var cacheFiles = [
     '/',
+    '/index.html',
     '/restaurant.html',
-    '/js/index.js',
-    '/js/dbhelper.js',
     '/js/main.js',
+    '/js/dbhelper.js',
     '/js/restaurant_info.js',
     '/css/styles.css',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg'
 ];
 
 const version = '0.6.11';
@@ -19,7 +29,7 @@ self.addEventListener('install', e => {
     e.waitUntil(
         caches.open(cacheName).then(cache => {
             return cache.addAll(cacheFiles).then(() => self.skipWaiting());
-        }),
+        })
     );
 });
 
@@ -31,12 +41,13 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
     console.log('Service worker fetching');
+    console.log({ event: event.request });
     event.respondWith(
         caches
             .open(cacheName)
             .then(cache => cache.match(event.request, { ignoreSearch: true }))
             .then(response => {
                 return response || fetch(event.request);
-            }),
+            })
     );
 });
