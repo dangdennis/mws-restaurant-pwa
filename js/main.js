39d265169af1ea72d3e9eb7cbb316068dc10b14c
Dennis Dang <dangggdennis@gmail.com>
2018-07-08 00:17:09 -0700

    update aria and alt based on review

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ var markers = [];
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
-document.addEventListener('DOMContentLoaded', event => {
+document.addEventListener("DOMContentLoaded", event => {
     fetchNeighborhoods();
-    console.log('initializing page');
+    console.log("initializing page");
     fetchCuisines();
 });
 
@@ -30,9 +30,9 @@ fetchNeighborhoods = () => {
  * Set neighborhoods HTML.
  */
 fillNeighborhoodsHTML = (neighborhoods = self.neighborhoods) => {
-    const select = document.getElementById('neighborhoods-select');
+    const select = document.getElementById("neighborhoods-select");
     neighborhoods.forEach(neighborhood => {
-        const option = document.createElement('option');
+        const option = document.createElement("option");
         option.innerHTML = neighborhood;
         option.value = neighborhood;
         select.append(option);
@@ -58,10 +58,10 @@ fetchCuisines = () => {
  * Set cuisines HTML.
  */
 fillCuisinesHTML = (cuisines = self.cuisines) => {
-    const select = document.getElementById('cuisines-select');
+    const select = document.getElementById("cuisines-select");
 
     cuisines.forEach(cuisine => {
-        const option = document.createElement('option');
+        const option = document.createElement("option");
         option.innerHTML = cuisine;
         option.value = cuisine;
         select.append(option);
@@ -74,12 +74,12 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
 window.initMap = () => {
     let loc = {
         lat: 40.722216,
-        lng: -73.987501
+        lng: -73.987501,
     };
-    self.map = new google.maps.Map(document.getElementById('map'), {
+    self.map = new google.maps.Map(document.getElementById("map"), {
         zoom: 12,
         center: loc,
-        scrollwheel: false
+        scrollwheel: false,
     });
     updateRestaurants();
 };
@@ -88,8 +88,8 @@ window.initMap = () => {
  * Update page and map for current restaurants.
  */
 updateRestaurants = () => {
-    const cSelect = document.getElementById('cuisines-select');
-    const nSelect = document.getElementById('neighborhoods-select');
+    const cSelect = document.getElementById("cuisines-select");
+    const nSelect = document.getElementById("neighborhoods-select");
 
     const cIndex = cSelect.selectedIndex;
     const nIndex = nSelect.selectedIndex;
@@ -97,15 +97,19 @@ updateRestaurants = () => {
     const cuisine = cSelect[cIndex].value;
     const neighborhood = nSelect[nIndex].value;
 
-    DBHelper.fetchRestaurantByCuisineAndNeighborhood(cuisine, neighborhood, (error, restaurants) => {
-        if (error) {
-            // Got an error!
-            console.error(error);
-        } else {
-            resetRestaurants(restaurants);
-            fillRestaurantsHTML();
-        }
-    });
+    DBHelper.fetchRestaurantByCuisineAndNeighborhood(
+        cuisine,
+        neighborhood,
+        (error, restaurants) => {
+            if (error) {
+                // Got an error!
+                console.error(error);
+            } else {
+                resetRestaurants(restaurants);
+                fillRestaurantsHTML();
+            }
+        },
+    );
 };
 
 /**
@@ -114,8 +118,8 @@ updateRestaurants = () => {
 resetRestaurants = restaurants => {
     // Remove all restaurants
     self.restaurants = [];
-    const ul = document.getElementById('restaurants-list');
-    ul.innerHTML = '';
+    const ul = document.getElementById("restaurants-list");
+    ul.innerHTML = "";
 
     // Remove all map markers
     self.markers.forEach(m => m.setMap(null));
@@ -127,7 +131,7 @@ resetRestaurants = restaurants => {
  * Create all restaurants HTML and add them to the webpage.
  */
 fillRestaurantsHTML = (restaurants = self.restaurants) => {
-    const ul = document.getElementById('restaurants-list');
+    const ul = document.getElementById("restaurants-list");
     restaurants.forEach(restaurant => {
         ul.append(createRestaurantHTML(restaurant));
     });
@@ -138,30 +142,29 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  * Create restaurant HTML.
  */
 createRestaurantHTML = restaurant => {
-    const li = document.createElement('li');
-    li.setAttribute('aria-label', 'restaurant');
+    const li = document.createElement("li");
+    li.setAttribute("aria-label", "restaurant");
 
-    const image = document.createElement('img');
-    image.className = 'restaurant-img';
+    const image = document.createElement("img");
+    image.className = "restaurant-img";
     image.src = DBHelper.imageUrlForRestaurant(restaurant);
-    image.alt = restaurant.name;
-    // image.alt =
+    image.alt = `Image of ${restaurant.name} Restaurant`;
     li.append(image);
 
-    const name = document.createElement('h1');
+    const name = document.createElement("h2");
     name.innerHTML = restaurant.name;
     li.append(name);
 
-    const neighborhood = document.createElement('p');
+    const neighborhood = document.createElement("p");
     neighborhood.innerHTML = restaurant.neighborhood;
     li.append(neighborhood);
 
-    const address = document.createElement('p');
+    const address = document.createElement("p");
     address.innerHTML = restaurant.address;
     li.append(address);
 
-    const more = document.createElement('a');
-    more.innerHTML = 'View Details';
+    const more = document.createElement("a");
+    more.innerHTML = "View Details";
     more.href = DBHelper.urlForRestaurant(restaurant);
     li.append(more);
 
@@ -175,7 +178,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     restaurants.forEach(restaurant => {
         // Add marker to the map
         const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
-        google.maps.event.addListener(marker, 'click', () => {
+        google.maps.event.addListener(marker, "click", () => {
             window.location.href = marker.url;
         });
         self.markers.push(marker);
